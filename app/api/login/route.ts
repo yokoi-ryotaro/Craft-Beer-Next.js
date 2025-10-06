3dba74540962442355fa630ee679152117b2e73a
yokoi-ryotaro <ryotaro.y0603@gmail.com>
2025-10-06 21:40:46 +0900

    10th commit

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -37,6 +37,7 @@ export async function POST(req: Request) {
   res.cookies.set(cookieName, token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: "lax", 
     path: "/",
     maxAge: 60 * 60 * 24 * 7, // 7日
   });
